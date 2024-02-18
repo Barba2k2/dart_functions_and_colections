b805258ce7700b0dabf1e5e6376572e1d6f297ed
Barba2k2 <lorenzo.dz@hotmail.com>
2024-02-18 14:09:31 -0300

    Porject Start

---
+++ bin/dart_functions_and_colections.dart
@@ -0,0 +1,5 @@
+import 'package:dart_functions_and_colections/dart_functions_and_colections.dart' as dart_functions_and_colections;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_functions_and_colections.calculate()}!');
+}
---
+++ test/dart_functions_and_colections_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_functions_and_colections/dart_functions_and_colections.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

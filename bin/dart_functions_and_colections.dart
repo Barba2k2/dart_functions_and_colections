e9afb332631ae5dc87367ff38498b634c624ae38
Barba2k2 <lorenzo.dz@hotmail.com>
2024-02-18 14:17:27 -0300

    FIX: Remove unused files

--- bin/dart_functions_and_colections.dart
+++
@@ -1,5 +0,0 @@
-import 'package:dart_functions_and_colections/dart_functions_and_colections.dart' as dart_functions_and_colections;
-
-void main(List<String> arguments) {
-  print('Hello world: ${dart_functions_and_colections.calculate()}!');
-}
--- test/dart_functions_and_colections_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dart_functions_and_colections/dart_functions_and_colections.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
